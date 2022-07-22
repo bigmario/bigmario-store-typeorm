88d1289968709021791ae54ec87f917c1e164ce0
Mario Castro <mariocastro.pva@gmail.com>
2022-07-22 15:44:51 -0400

    entities

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,8 +6,13 @@ import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Brand } from './entities/brand.entity';
+import { Category } from './entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Brand, Category])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
