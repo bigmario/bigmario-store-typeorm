c4fb7d9f0227a1482cbdd32f2dd9131503b1d62d
Mario Castro <mariocastro.pva@gmail.com>
2022-10-06 10:12:01 -0400

    entities

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { ProductsController } from './controllers/products.controller';
-import { BrandsController } from './controllers/brands.controller';
-import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
-import { BrandsService } from './services/brands.service';
-import { CategoriesService } from './services/categories.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { BrandsController } from './controllers/brands.controller';
+import { BrandsService } from './services/brands.service';
 import { Brand } from './entities/brand.entity';
+import { CategoriesController } from './controllers/categories.controller';
+import { CategoriesService } from './services/categories.service';
 import { Category } from './entities/category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product, Brand, Category])],
--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,8 +1,15 @@
 import { User } from './user.entity';
 import { Product } from './../../products/entities/product.entity';
+import { Column, Entity } from 'typeorm';
 
+@Entity()
 export class Order {
+  @Column({ type: 'varchar', length: 255 })
   date: Date;
+
+  @Column()
   user: User;
+
+  @Column()
   products: Product[];
 }
