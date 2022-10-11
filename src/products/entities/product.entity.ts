403e28cd40a2b38af66e224233eabf69f132108d
Mario Castro <mariocastro.pva@gmail.com>
2022-10-11 15:40:10 -0400

    indexing

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,11 +7,13 @@ import {
   ManyToOne,
   ManyToMany,
   JoinTable,
+  Index,
 } from 'typeorm';
 
 import { Brand } from './brand.entity';
 import { Category } from './category.entity';
 @Entity()
+@Index(['price', 'stock'])
 export class Product {
   @PrimaryGeneratedColumn()
   id: number;
@@ -22,6 +24,7 @@ export class Product {
   @Column({ type: 'text' })
   description: string;
 
+  @Index()
   @Column({ type: 'int' })
   price: number;
 
